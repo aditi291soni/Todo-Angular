69ed67d331fb9f9729dc8a3016e28cfaad164210
prajakta.kadam@urspayce.com <prajakta.kadam@urspayce.com>
2023-04-05 16:54:28 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolist';
+  title = 'Todo-list';
+  list:any[]=[]
+  gettask(item:string){
+    this.list.push({id:this.list.length+1,name:item})
+    console.log(this.list);
+  }
+  removeitem(id:number){
+    
+    this.list=this.list.filter(item=>item.id!==id)
+  }
 }
